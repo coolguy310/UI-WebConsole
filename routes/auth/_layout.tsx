b4d058556e64c947d6f24da505f1cbab9aca56c3
harshmangalam <harshdev8218@gmail.com>
2023-09-05 14:59:18 +0530

    added layout helper

--- routes/auth/_layout.tsx
+++ routes/auth/_layout.tsx
@@ -1,11 +1,12 @@
-import { LayoutProps, RouteConfig } from "$fresh/server.ts";
+import { RouteConfig } from "$fresh/server.ts";
+import { defineLayout } from "$fresh/src/server/defines.ts";
 import { Footer } from "./(_components)/footer.tsx";
 import { VectorBackground } from "./(_components)/vector-background.tsx";
 import { Button } from "@/components/ui/button/index.tsx";
 
-export default function LandingPageLayout({ Component, route }: LayoutProps) {
-  const isLoginRoute = route.includes("/auth/login");
-  const isRegisterRoute = route.includes("/auth/register");
+export default defineLayout((_, ctx) => {
+  const isLoginRoute = ctx.route.includes("/auth/login");
+  const isRegisterRoute = ctx.route.includes("/auth/register");
   return (
     <>
       <VectorBackground />
@@ -34,7 +35,7 @@ export default function LandingPageLayout({ Component, route }: LayoutProps) {
         <div class="flex-1 h-full mx-auto max-w-[511px] w-full grid place-items-center">
           <div class="w-full z-50">
             <div className="bg-[#28282B] rounded-3xl px-6 py-8 flex flex-col space-y-5 w-full">
-              <Component />
+              <ctx.Component />
             </div>
             <Footer />
           </div>
@@ -42,8 +43,7 @@ export default function LandingPageLayout({ Component, route }: LayoutProps) {
       </div>
     </>
   );
-}
-
+});
 export const config: RouteConfig = {
   skipInheritedLayouts: true,
 };
