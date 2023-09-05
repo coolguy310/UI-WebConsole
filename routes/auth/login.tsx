9f4d192d83f5d2ad68caee50067dbba4659ef5e5
harshmangalam <harshdev8218@gmail.com>
2023-09-05 13:27:34 +0530

    reduce auth login islands payload

--- routes/auth/login.tsx
+++ routes/auth/login.tsx
@@ -1,14 +1,18 @@
 import { PageProps } from "$fresh/server.ts";
 import AuthLoginForm from "@/islands/form-auth-login.tsx";
-import { VectorBackground } from "./(_components)/vector-background.tsx";
-import { AuthLayout } from "@/components/layouts/auth.tsx";
 
 export default function AuthLogin(props: PageProps) {
   return (
     <>
-      <VectorBackground />
-      <div className="relative z-10">
-        <AuthLoginForm />
+      <h1 className="text-2xl font-bold text-center">Login to your Account</h1>
+      <AuthLoginForm />
+      <div className="text-center text-stroke text-sm">
+        <div className="mt-3">
+          <span>Not having an account yet?</span>{" "}
+          <a href="/auth/register" className="font-semibold text-accent">
+            Register
+          </a>
+        </div>
       </div>
     </>
   );
