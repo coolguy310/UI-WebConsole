1782b7cfa6aaec06e12522820d465db95a8867ca
harshmangalam <harshdev8218@gmail.com>
2023-09-05 10:48:31 +0530

    removed unused code

--- routes/auth/_layout.tsx
+++ routes/auth/_layout.tsx
@@ -1,11 +1,7 @@
 import { LayoutProps } from "$fresh/server.ts";
 import { AuthLayout } from "@/components/layouts/auth.tsx";
 
-export default function LandingPageLayout({ Component, route }: LayoutProps) {
-  const showLoginButton = route === "/register";
-  const showRegisterButton = false;
-  const hideBottomStroke = false;
-
+export default function LandingPageLayout({ Component }: LayoutProps) {
   return (
     <AuthLayout>
       <Component />
