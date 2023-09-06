3821264f398674b671e40073268219890e53f960
harshmangalam <harshdev8218@gmail.com>
2023-09-06 23:05:00 +0530

    removed unused layout

--- routes/console/logs/index.tsx
+++ routes/console/logs/index.tsx
@@ -1,13 +1,11 @@
-import { PageProps } from "$fresh/src/server/types.ts"
-
-import { ConsoleLayout } from "../../../components/layouts/console.tsx"
-import LogsViewTable from "../../../components/misc/table-logs-view.tsx"
+import { PageProps } from "$fresh/src/server/types.ts";
+import LogsViewTable from "../../../components/misc/table-logs-view.tsx";
 
 export default function SecretsPage(props: PageProps) {
   return (
-    <ConsoleLayout location={props.url} className="flex flex-col space-y-8">
+    <>
       <h1 className="text-2xl font-semibold">Logs View</h1>
       <LogsViewTable title="50 out of 20k" />
-    </ConsoleLayout>
-  )
+    </>
+  );
 }
--- routes/console/settings/index.tsx
+++ routes/console/settings/index.tsx
@@ -1,21 +1,22 @@
-import { PageProps } from "$fresh/src/server/types.ts"
-import IconTrash from "icons/trash.tsx"
-
-import { ConsoleLayout } from "../../../components/layouts/console.tsx"
-import { Button } from "../../../components/ui/button/index.tsx"
-import SettingsPage from "../../../islands/settings.tsx"
+import { PageProps } from "$fresh/src/server/types.ts";
+import IconTrash from "icons/trash.tsx";
+import { Button } from "../../../components/ui/button/index.tsx";
+import SettingsPage from "../../../islands/settings.tsx";
 
 export default function SettingsMasterPage(props: PageProps) {
   return (
-    <ConsoleLayout location={props.url} className="flex flex-col space-y-8">
+    <>
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-semibold">Settings</h1>
-        <Button variant="danger" className="text-sm flex items-center space-x-1 !py-3 font-semibold">
+        <Button
+          variant="danger"
+          className="text-sm flex items-center space-x-1 !py-3 font-semibold"
+        >
           <IconTrash size={16} />
           <span>Delete Account</span>
         </Button>
       </div>
       <SettingsPage />
-    </ConsoleLayout>
-  )
+    </>
+  );
 }
